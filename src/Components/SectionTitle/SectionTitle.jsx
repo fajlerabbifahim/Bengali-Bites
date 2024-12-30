81086d3828f9b651d9f65a98b0e653515ada1fd7
Yourfaahim <fahimqw1@gmail.com.com>
2024-12-30 11:30:25 +0600

    heading and sub heading added useing sectionTitle components

---
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function SectionTitle({ subTitle, title }) {
+  return (
+    <div className="text-center mx-auto w-4/12">
+      <p className="text-yellow-600 font-medium text-lg italic">
+        ---{subTitle}---
+      </p>
+      <h1 className="uppercase text-4xl font-semibold border-t-4 border-y-4 border-gray-500 py-4 my-8">
+        {title}
+      </h1>
+    </div>
+  );
+}
+
+export default SectionTitle;
--- src/Pages/Home/CategorySlider/CategorySlider.jsx
+++ src/Pages/Home/CategorySlider/CategorySlider.jsx
@@ -6,10 +6,15 @@ import slide2 from "../../../assets/home/slide2.jpg";
 import slide3 from "../../../assets/home/slide3.jpg";
 import slide4 from "../../../assets/home/slide4.jpg";
 import slide5 from "../../../assets/home/slide5.jpg";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 
 function CategorySlider() {
   return (
     <div className="w-11/12 mx-auto my-20">
+      <SectionTitle
+        subTitle={"From 11:00am to 10:00pm"}
+        title={"ORDER ONLINE"}
+      />
       <Swiper
         slidesPerView={1}
         spaceBetween={10}
