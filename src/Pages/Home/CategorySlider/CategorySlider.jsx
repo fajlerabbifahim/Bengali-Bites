f592de110cb9278e4c6da03a280d2c634cd7beec
Yourfaahim <fahimqw1@gmail.com.com>
2024-12-30 10:50:54 +0600

    category slider added

---
+++ src/Pages/Home/CategorySlider/CategorySlider.jsx
@@ -0,0 +1,58 @@
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import slide1 from "../../../assets/home/slide1.jpg";
+import slide2 from "../../../assets/home/slide2.jpg";
+import slide3 from "../../../assets/home/slide3.jpg";
+import slide4 from "../../../assets/home/slide4.jpg";
+import slide5 from "../../../assets/home/slide5.jpg";
+
+function CategorySlider() {
+  return (
+    <div className="w-11/12 mx-auto my-20">
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={10}
+        pagination={true}
+        breakpoints={{
+          320: { slidesPerView: 2, spaceBetween: 20 },
+          480: { slidesPerView: 3, spaceBetween: 10 },
+          640: { slidesPerView: 4, spaceBetween: 10 },
+        }}
+      >
+        <SwiperSlide>
+          <img src={slide1} alt="" />
+          <p className="text-white text-center text-4xl font-semibold -mt-16 uppercase">
+            Salad
+          </p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide2} alt="" />
+          <p className="text-white text-center text-4xl font-semibold -mt-16 uppercase">
+            pizza
+          </p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide3} alt="" />
+          <p className="text-white text-center text-4xl font-semibold -mt-16 uppercase">
+            Pinky
+          </p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide4} alt="" />
+          <p className="text-white text-center text-4xl font-semibold -mt-16 uppercase">
+            cake
+          </p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide5} alt="" />
+          <p className="text-white text-center text-4xl font-semibold -mt-16 uppercase">
+            Salad
+          </p>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
+
+export default CategorySlider;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Banner from "../Banner/Banner";
+import CategorySlider from "../CategorySlider/CategorySlider";
 
 function Home() {
   return (
     <div>
       <section>
         <Banner />
+        <CategorySlider />
       </section>
     </div>
   );
